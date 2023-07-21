53799236d28d2a3fac2171099228e9d6c007a429
MatveikaSW <mets0n18@vk.com>
2023-07-21 16:05:53 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Матвей Авдеев on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
